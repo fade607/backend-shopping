1c6d936d89f5795b7b4003f154ffa01a5ccf3b5f
fade ahmad <ffaaddee607122@gmail.com>
2021-09-30 02:26:46 +0530

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-73=n_dk@akbk0ko=rdj08h*q5+9x_!!yttk8q%e#y^_&5#36qy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fade-ahm.herokuapp.com','127.0.0.1']
 
 
 # Application definition
